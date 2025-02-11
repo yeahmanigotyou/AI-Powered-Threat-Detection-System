10b0a906dff84f477674c3512de63d82936fc005
yeahmanigotyou <jvsingh254@gmail.com>
2025-02-10 21:57:00 -0500

    changed tshark to pyshark and added formatting util

--- src/tools/tshark_wrapper.py
+++ src/tools/tshark_wrapper.py
@@ -1,25 +1,24 @@
-import subprocess
-from pathlib import Path
-import logging
+import pyshark
+from typing import List, Dict 
+from utils.packet_formatting import PacketFormatter
 
 class TsharkWrapper:
-    def __init__(self, interface):
-        self.interface = interface
-        self.logger = logging.getLogger(__name__)
-        
-    def capture_traffic(self, duration):
-            output_file = Path(f"data/capture_{datetime.now():%Y%m%d_%H%M%S}.pcap")
-            
-            cmd = [
-                 "tshark",
-                 "-i", self.interface,
-                 "-w", str(output_file),
-                 "-a", f"duration:{duration}"
-            ]
-            
-            try:
-                 subprocess.run(cmd, check=True)
-                 return output_file
-            except subprocess.CalledProcessError as e:
-                 self.logger.error(f"Captured Failed: {e}")
-                 raise
+     def __init__(self):
+          self.default_options = ['-T', 'json']
+          self.format = PacketFormatter()
+
+     def capture_packets(self, interface: str, duration: int = 60) -> List[Dict]:
+         try:
+              capture = pyshark.LiveCapture(interface=interface)
+              packets = []
+              
+              for packet in capture.sniff_continuously(packet_count=1000):
+                   packet_dict = self.format._convert_packet_to_dict_(packet)
+                   packets.append(packet_dict)
+                   
+                   if len(packets) >= 1000:
+                        break
+                   
+              return packets
+         except Exception as e:
+              raise Exception(f"Failed to Capture Packets: {str(e)}")
